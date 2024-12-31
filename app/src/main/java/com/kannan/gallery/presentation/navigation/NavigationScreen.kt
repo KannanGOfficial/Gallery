d53095cb1d16114080d5553fa4f153fdf243acc7
KannanGOfficial <kannang143592@gmail.com>
2024-12-31 19:04:27 +0530

    Finishes Screen Names

--- app/src/main/java/com/kannan/gallery/presentation/navigation/NavigationScreen.kt
+++ app/src/main/java/com/kannan/gallery/presentation/navigation/NavigationScreen.kt
@@ -8,23 +8,14 @@ sealed interface NavigationScreen {
     data object SetupScreen : NavigationScreen
 
     @Serializable
-    data object TimelineScreen : NavigationScreen
-
-    @Serializable
-    data class TimelineMediaScreen(val initialPagerPosition: Int) : NavigationScreen
+    data object PhotoScreen : NavigationScreen
 
     @Serializable
     data object AlbumScreen : NavigationScreen
 
     @Serializable
-    data class AlbumTimeLineScreen(val albumName: String) : NavigationScreen
-
-    @Serializable
-    data class AlbumMediaScreen(val initialPagerPosition: Int) : NavigationScreen
+    data class AlbumDetailScreen(val albumName: String) : NavigationScreen
 
     @Serializable
     data object SettingsScreen : NavigationScreen
-
-    @Serializable
-    data class MemoriesScreen(val albumName: String? = null) : NavigationScreen
 }
