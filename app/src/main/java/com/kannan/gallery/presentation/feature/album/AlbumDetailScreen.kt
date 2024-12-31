9f4abc8ee236053b1803de0f3f72324fe4a2de34
KannanGOfficial <kannang143592@gmail.com>
2024-12-31 21:48:22 +0530

    Finishes AlbumDetailScreen Implementation

--- app/src/main/java/com/kannan/gallery/presentation/feature/album/AlbumDetailScreen.kt
+++ app/src/main/java/com/kannan/gallery/presentation/feature/album/AlbumDetailScreen.kt
@@ -1,25 +1,66 @@
 package com.kannan.gallery.presentation.feature.album
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kannan.gallery.domain.model.Media
+import com.kannan.gallery.presentation.components.MediaContent
+import com.kannan.gallery.presentation.components.TimelineContent
+import com.kannan.gallery.presentation.feature.photo.ScreenContentType
+import com.kannan.gallery.presentation.main.dummyTimelineMediaList
 import com.kannan.gallery.ui.theme.GalleryTheme
+import com.kannan.gallery.utils.ext.CollectAsEffect
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 @Composable
-fun AlbumDetailScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.then(Modifier.fillMaxSize()), contentAlignment = Alignment.Center) {
-        Text(text = "AlbumDetailScreen")
+fun AlbumDetailScreen(
+    modifier: Modifier = Modifier,
+    uiState: AlbumDetailScreenUiState,
+    uiEvent: Flow<AlbumDetailScreenUiEvent>,
+    uiAction: (AlbumDetailScreenUiAction) -> Unit,
+    mediaList: List<Media>,
+    navigateUpCallback: () -> Unit
+) {
+    uiEvent.CollectAsEffect { event ->
+        when (event) {
+            AlbumDetailScreenUiEvent.NavigateUp -> navigateUpCallback.invoke()
+        }
     }
+
+    TimelineContent(
+        modifier = modifier,
+        mediaList = mediaList,
+        currentMediaPosition = uiState.currentMediaPosition,
+        onImageClicked = { uiAction.invoke(AlbumDetailScreenUiAction.OnImageClicked(it)) },
+        onImageLongClicked = { uiAction.invoke(AlbumDetailScreenUiAction.OnImageLongClicked(it)) },
+        onBackPressed = { uiAction.invoke(AlbumDetailScreenUiAction.OnTimelineContentBackPressed) },
+    )
+
+    if (uiState.screenContentType == ScreenContentType.MEDIA) {
+        MediaContent(
+            mediaList = mediaList,
+            initialPagerPosition = uiState.currentMediaPosition,
+            modifier = modifier,
+            onBackPressed = { uiAction.invoke(AlbumDetailScreenUiAction.OnMediaContentBackPressed(it)) }
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun AlbumDetailScreenPreview() {
     GalleryTheme {
-        AlbumDetailScreen()
+        AlbumDetailScreen(
+            uiState = AlbumDetailScreenUiState(
+                screenContentType = ScreenContentType.MEDIA
+            ),
+            uiEvent = emptyFlow(),
+            uiAction = {},
+            mediaList = dummyTimelineMediaList,
+            navigateUpCallback = {}
+        )
     }
 }
