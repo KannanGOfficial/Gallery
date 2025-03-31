830cfb595c30781285aa1f44d858e0568778bc8a
Kannan G <103372148+KannanGOfficial@users.noreply.github.com>
2025-03-31 09:01:16 +0530

    Finishes Filter out hidden images. (#25)

--- app/src/main/java/com/kannan/gallery/data/contentResolver/GetAllAlbum.kt
+++ app/src/main/java/com/kannan/gallery/data/contentResolver/GetAllAlbum.kt
@@ -6,12 +6,12 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import com.kannan.gallery.data.contentResolver.models.AlbumCR
+import com.kannan.gallery.utils.StringUtil
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-// TODO: Need to filter out the Hidden files
 class GetAllAlbum @Inject constructor(@ApplicationContext private val context: Context) {
     private val queryUri =
         MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL)
@@ -19,6 +19,7 @@ class GetAllAlbum @Inject constructor(@ApplicationContext private val context: C
     private val projection = arrayOf(
         MediaStore.MediaColumns.BUCKET_ID,
         MediaStore.MediaColumns.BUCKET_DISPLAY_NAME,
+        MediaStore.MediaColumns.RELATIVE_PATH,
         MediaStore.MediaColumns.DATA
     )
 
@@ -53,23 +54,35 @@ class GetAllAlbum @Inject constructor(@ApplicationContext private val context: C
             )
 
             cursor?.use {
-                val bucketIdX = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.BUCKET_ID)
-                val bucketNameX =
-                    cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.BUCKET_DISPLAY_NAME)
-                val dataX = cursor.getColumnIndex(MediaStore.MediaColumns.DATA)
+                with(cursor) {
 
-                while (cursor.moveToNext()) {
-                    val bucketId = cursor.getLong(bucketIdX)
-                    val bucketName = cursor.getString(bucketNameX)
-                    val data = cursor.getString(dataX)
+                    val bucketIdX = getColumnIndexOrThrow(MediaStore.MediaColumns.BUCKET_ID)
+                    val bucketNameX =
+                        getColumnIndexOrThrow(MediaStore.MediaColumns.BUCKET_DISPLAY_NAME)
+                    val dataX = getColumnIndex(MediaStore.MediaColumns.DATA)
+                    val relativePathX =
+                        getColumnIndexOrThrow(MediaStore.MediaColumns.RELATIVE_PATH)
 
-                    val albumCR = AlbumCR(
-                        id = bucketId,
-                        name = bucketName ?: Build.MODEL,
-                        coverImage = data
-                    )
+                    while (moveToNext()) {
+                        val bucketId = getLong(bucketIdX)
+                        val bucketName = getString(bucketNameX)
+                        val data = getString(dataX)
+                        val relativePath = getString(relativePathX)
 
-                    albumList.add(albumCR)
+                        if (StringUtil.pathStartsWithDot(data)) {
+                            // Skip hidden files or files inside hidden folders
+                            continue
+                        }
+
+                        val albumCR = AlbumCR(
+                            id = bucketId,
+                            name = bucketName ?: Build.MODEL,
+                            coverImage = data,
+                            relativePath = relativePath
+                        )
+
+                        albumList.add(albumCR)
+                    }
                 }
             }
 
