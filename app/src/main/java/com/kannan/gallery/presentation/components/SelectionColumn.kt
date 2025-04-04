ef9760e8ce3337293695764fc3008673ad30d515
Kannan G <103372148+KannanGOfficial@users.noreply.github.com>
2025-04-04 13:24:42 +0530

    Finishes Selection Sheet Ui (#29)

---
+++ app/src/main/java/com/kannan/gallery/presentation/components/SelectionColumn.kt
@@ -0,0 +1,71 @@
+package com.kannan.gallery.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SelectionColumn(
+    imageVector: ImageVector,
+    title: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    val tintColor = MaterialTheme.colorScheme.onSurface
+
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(12.dp))
+            .clickable(onClick = onClick),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            imageVector = imageVector,
+            colorFilter = ColorFilter.tint(tintColor),
+            contentDescription = title,
+            modifier = Modifier
+                .height(32.dp)
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+            text = title,
+            modifier = Modifier,
+            fontWeight = FontWeight.Medium,
+            style = MaterialTheme.typography.bodyMedium,
+            color = tintColor,
+            textAlign = TextAlign.Center
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SelectionColumnPreview() {
+    SelectionColumn(
+        imageVector = Icons.Outlined.Share,
+        title = "Share",
+        onClick = {},
+
+        )
+}
--- app/src/main/java/com/kannan/gallery/presentation/components/TimelineContent.kt
+++ app/src/main/java/com/kannan/gallery/presentation/components/TimelineContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -13,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,44 +36,62 @@ fun SharedTransitionScope.TimelineContent(
     onImageLongClicked: (Media) -> Unit,
     onBackPressed: () -> Unit,
     isInMediaSelectionMode: Boolean,
+    selectedItemCount: Int,
     animatedVisibilityScope: AnimatedVisibilityScope
 ) {
 
     BackHandler(onBack = onBackPressed)
 
-    LazyVerticalGrid(
-        modifier = modifier.fillMaxSize(),
-        state = lazyGridState,
-        columns = GridCells.Fixed(3)
+    Box(
+        modifier = modifier.fillMaxSize()
     ) {
-        items(
-            count = lazyPagingItems.itemCount
-        ) { index: Int ->
-            val data = lazyPagingItems[index]
-            data?.let {
-                Thumbnail(
-                    modifier = Modifier
-                        .size(200.dp)
-                        .sharedBounds(
-                            sharedContentState = rememberSharedContentState(key = "image/ ${data.id}"),
-                            animatedVisibilityScope = animatedVisibilityScope,
-                            resizeMode = SharedTransitionScope.ResizeMode.ScaleToBounds()
-                        ),
-                    data = data.uri,
-                    contentDescription = data.uri,
-                    onClick = {
-                        onImageClicked.invoke(index, data)
-                    },
-                    onLongClick = {
-                        onImageLongClicked.invoke(data)
-                    },
-                    isSelected = data.isSelected,
-                    isInMediaSelectionMode = isInMediaSelectionMode
-                )
-            }
+        LazyVerticalGrid(
+            state = lazyGridState,
+            columns = GridCells.Fixed(3)
+        ) {
+            items(
+                count = lazyPagingItems.itemCount
+            ) { index: Int ->
+                val data = lazyPagingItems[index]
+                data?.let {
+                    Thumbnail(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .sharedBounds(
+                                sharedContentState = rememberSharedContentState(key = "image/ ${data.id}"),
+                                animatedVisibilityScope = animatedVisibilityScope,
+                                resizeMode = SharedTransitionScope.ResizeMode.ScaleToBounds()
+                            ),
+                        data = data.uri,
+                        contentDescription = data.uri,
+                        onClick = {
+                            onImageClicked.invoke(index, data)
+                        },
+                        onLongClick = {
+                            onImageLongClicked.invoke(data)
+                        },
+                        isSelected = data.isSelected,
+                        isInMediaSelectionMode = isInMediaSelectionMode
+                    )
+                }
 
+            }
+        }
+
+        if (isInMediaSelectionMode) {
+            SelectionSheet(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter),
+                selectedItemCount = selectedItemCount,
+                onCopyButtonClick = {},
+                onMoveButtonClick = {},
+                onCloseButtonClick = {},
+                onShareButtonClick = {}
+            )
         }
     }
+
+
 }
 
 @OptIn(ExperimentalSharedTransitionApi::class)
@@ -88,7 +108,8 @@ private fun TimelineContentPreview() {
                     animatedVisibilityScope = this,
                     lazyPagingItems = flowOf(PagingData.empty<Media>()).collectAsLazyPagingItems(),
                     lazyGridState = rememberLazyGridState(),
-                    isInMediaSelectionMode = false
+                    isInMediaSelectionMode = false,
+                    selectedItemCount = 2
                 )
             }
         }
